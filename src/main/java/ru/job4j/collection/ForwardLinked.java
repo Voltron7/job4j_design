35f540072b2446b625c9c883a93a666809d43c0f
Voltron7 <aquastar.minsk@gmail.com>
2022-07-28 19:08:59 +0300

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -35,6 +35,22 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    public boolean revert() {
+        boolean result = false;
+        if (head != null && head.next != null) {
+            Node<T> currentNode = head.next;
+            head.next = null;
+            while (currentNode != null) {
+                Node<T> nextNode = currentNode.next;
+                currentNode.next = head;
+                head = currentNode;
+                currentNode = nextNode;
+            }
+            result = true;
+        }
+        return result;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -10,17 +10,18 @@ import java.util.NoSuchElementException;
 class ForwardLinkedTest {
     private ForwardLinked<Integer> linked;
 
+
     @BeforeEach
     public void init() {
         linked = new ForwardLinked<>();
-        linked.add(1);
-        linked.add(2);
-        linked.add(3);
-        linked.add(4);
     }
 
     @Test
     void whenDeleteFirst() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
         assertThat(linked.deleteFirst()).isEqualTo(1);
         assertThat(linked.deleteFirst()).isEqualTo(2);
         assertThat(linked.deleteFirst()).isEqualTo(3);
@@ -38,8 +39,34 @@ class ForwardLinkedTest {
 
     @Test
     void whenMultiDelete() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
         linked.deleteFirst();
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next()).isEqualTo(2);
+    }
+
+    @Test
+    void whenSize0ThenReturnFalse() {
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenSize1ThenReturnFalse() {
+        linked.add(1);
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenAddAndRevertTrue() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
+        assertThat(linked).containsSequence(1, 2, 3, 4);
+        assertThat(linked.revert()).isTrue();
+        assertThat(linked).containsSequence(4, 3, 2, 1);
     }
 }
