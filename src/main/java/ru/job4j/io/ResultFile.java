1792b0a27f698aebb9348a13d169eb4946dbd394
Voltron7 <aquastar.minsk@gmail.com>
2022-09-07 12:46:39 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            String result;
+            for (int multiplier = 1; multiplier <= 10; multiplier++) {
+                out.write(System.lineSeparator().getBytes());
+                for (int multiplicand = 1; multiplicand <= 10; multiplicand++) {
+                    result = multiplier + "*" + multiplicand + "=" + (multiplier * multiplicand) + "\t";
+                    out.write(result.getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
