a4cda9dfecbcb096cec7d1dbe2e9e11755ccd4c6
Voltron7 <aquastar.minsk@gmail.com>
2022-09-07 14:11:12 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int num = Integer.parseInt(line);
+                boolean rsl = num % 2 == 0;
+                System.out.println("The number " + num + " is even: " + rsl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
