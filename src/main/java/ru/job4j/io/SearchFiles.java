95f6351b4ee20d2c7b060c1cf08bf3f7cf8c853c
Voltron7 <aquastar.minsk@gmail.com>
2022-09-14 15:05:39 +0300

    4.1. Сканирование файловой системы. [#106929 #343287]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -20,7 +20,7 @@ public class SearchFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            paths.add(file.toAbsolutePath());
+            paths.add(file);
         }
         return FileVisitResult.CONTINUE;
     }
