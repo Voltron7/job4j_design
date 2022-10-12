fa59175d392bc43605330bfcf6b96ca15752d3c2
Voltron7 <aquastar.minsk@gmail.com>
2022-10-12 15:31:57 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -0,0 +1,48 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        final Person person = new Person(false, 30, new Contact("11-111"),
+                new String[] {"Worker", "Married"});
+
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(person));
+
+        final String personJson =
+                "{"
+                        + "\"sex\":false,"
+                        + "\"age\":35,"
+                        + "\"contact\":"
+                        + "{"
+                        + "\"phone\":\"+7(924)111-111-11-11\""
+                        + "},"
+                        + "\"statuses\":"
+                        + "[\"Student\",\"Free\"]"
+                        + "}";
+        final Person personMod = gson.fromJson(personJson, Person.class);
+        System.out.println(personMod);
+
+        final Person2 person2 = new Person2("Valeri", true, 39, new Contact("77-777"),
+                 new String[] {"Spring, Hibernate"});
+        final Gson gson2 = new GsonBuilder().create();
+        System.out.println(gson2.toJson(person2));
+
+        final String personJson2 =
+                "{"
+                        + "\"name\": Pavel,"
+                        + "\"sex\":true,"
+                        + "\"age\":42,"
+                        + "\"contact\":"
+                        + "{"
+                        + "\"phone\":\"+7(911)777-777-77-77\""
+                        + "},"
+                        + "\"statuses\":"
+                        + "[\"Junior\",\"FreeSoul\"]"
+                        + "}";
+        final Person2 personMod2 = gson2.fromJson(personJson2, Person2.class);
+        System.out.println(personMod2);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/Person2.java
@@ -0,0 +1,31 @@
+package ru.job4j.serialization.json;
+
+import java.util.Arrays;
+
+public class Person2 {
+    private final String name;
+    private final boolean sex;
+    private final int age;
+    private final Contact contact;
+    private final String[] statuses;
+
+
+    public Person2(String name, boolean sex, int age, Contact contact, String[] skills) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "Person2{"
+                + "name=" + name
+                + ", sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+}
