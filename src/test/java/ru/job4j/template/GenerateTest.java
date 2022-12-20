5a16f873aef68815fcb2304b147aade1e6f3654d
Voltron7 <aquastar.minsk@gmail.com>
2022-12-20 09:28:22 +0300

    3. Шаблонизатор. [#855 #372710]

--- src/test/java/ru/job4j/template/GenerateTest.java
+++ src/test/java/ru/job4j/template/GenerateTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Disabled
 class GenerateTest {
@@ -23,9 +22,8 @@ class GenerateTest {
         Generator generator = new Generate();
         String template = "I am a ${name} ${surname}, Who are ${subject}? ";
         Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
-        assertThrows(IllegalArgumentException.class, () -> {
-            generator.produce(template, args);
-        });
+        assertThatThrownBy(() -> generator.produce(template, args)).
+                isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -34,8 +32,7 @@ class GenerateTest {
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you",
                 "surname", "Arsentev");
-        assertThrows(IllegalArgumentException.class, () -> {
-            generator.produce(template, args);
-        });
+        assertThatThrownBy(() -> generator.produce(template, args)).
+                isInstanceOf(IllegalArgumentException.class);
     }
 }
