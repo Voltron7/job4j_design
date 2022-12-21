be29e3dd860bf3ee149391cdff6891a270c9c0b1
Voltron7 <aquastar.minsk@gmail.com>
2022-12-21 18:12:36 +0300

    1. Отчеты. [#850]

---
+++ src/main/java/ru/job4j/ood/srp/report/ReportForBookkeeping.java
@@ -0,0 +1,40 @@
+package ru.job4j.ood.srp.report;
+
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.currency.CurrencyConverter;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.Store;
+
+import java.util.Calendar;
+import java.util.function.Predicate;
+
+public class ReportForBookkeeping implements Report {
+    private final Store store;
+    private final DateTimeParser<Calendar> dateTimeParser;
+    private final CurrencyConverter currencyConverter;
+
+    public ReportForBookkeeping(Store store, DateTimeParser<Calendar> dateTimeParser,
+                                CurrencyConverter currencyConverter) {
+        this.store = store;
+        this.dateTimeParser = dateTimeParser;
+        this.currencyConverter = currencyConverter;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        StringBuilder text = new StringBuilder();
+        text.append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator());
+        for (Employee employee : store.findBy(filter)) {
+            employee.setSalary(currencyConverter.convert(
+                    Currency.RUB, employee.getSalary(), Currency.EUR));
+            text.append(employee.getName()).append(" ")
+                    .append(dateTimeParser.parse(employee.getHired())).append(" ")
+                    .append(dateTimeParser.parse(employee.getFired())).append(" ")
+                    .append(employee.getSalary())
+                    .append(System.lineSeparator());
+        }
+        return text.toString();
+    }
+}
