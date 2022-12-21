42ef94b93abd30ce1356f8669d2a3ea6f4a190a4
Voltron7 <aquastar.minsk@gmail.com>
2022-12-21 11:26:58 +0300

    0. Принцип единственной ответственности [#4913 #373059]

--- src/main/java/ru/job4j/ood/srp/violation/Person.java
+++ src/main/java/ru/job4j/ood/srp/violation/Person.java
@@ -14,6 +14,11 @@ public class Person implements People {
         this.created = created;
     }
 
+    /*
+    Нарушение SRP из-за того, что класс умеет создавать и инициализировать объект.
+    Нарушение SRP из-за того, что, например, используется дата в определенном формате,
+    который может поменяться.
+     */
     public Person createPerson() {
         LocalDateTime created = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
@@ -21,10 +26,18 @@ public class Person implements People {
         return new Person("Valeri", "Shpakowski", createdFormat);
     }
 
+    /*
+    Нарушение SRP из-за того, что большое количество публичных методов класса.
+     */
     public String createInfo(Person person) {
         return person.name + " " + person.surname + " " + person.created;
     }
 
+    /*
+    Нарушение SRP из-за того, что у класса несколько целей, а должна быть одна.
+    Например, Цель метода speak возвращать "Hello", а метода print выводить любую
+    строку в консоль.
+     */
     @Override
     public String speak() {
         return "Hello";
