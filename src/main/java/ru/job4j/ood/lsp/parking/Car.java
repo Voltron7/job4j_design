7bfa7c0a25d417306f42e19725dcb148942f8699
Voltron7 <aquastar.minsk@gmail.com>
2023-01-05 18:21:20 +0300

    2. Парковка машин [#853 #377834]

--- src/main/java/ru/job4j/ood/lsp/parking/Car.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Car.java
@@ -2,4 +2,27 @@ package ru.job4j.ood.lsp.parking;
 
 public class Car extends Vehicle {
 
+    public Car(String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
 }
--- src/main/java/ru/job4j/ood/lsp/parking/Truck.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Truck.java
@@ -2,4 +2,27 @@ package ru.job4j.ood.lsp.parking;
 
 public class Truck extends Vehicle {
 
+    public Truck(String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
 }
