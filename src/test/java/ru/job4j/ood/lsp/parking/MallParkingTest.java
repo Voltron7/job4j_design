85818aafbae773afffb33ec2b97b9d53c1bbcb73
Voltron7 <aquastar.minsk@gmail.com>
2023-01-18 20:16:40 +0300

    2. Парковка машин [#853 #377834]

--- src/test/java/ru/job4j/ood/lsp/parking/MallParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/MallParkingTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.ood.lsp.parking;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
+@Disabled
 class MallParkingTest {
 
     @Test
@@ -11,49 +13,38 @@ class MallParkingTest {
         MallParking mallParking = new MallParking(1, 1);
         Vehicle truck = new Truck("Truck", 2);
         Vehicle car = new Car("Car");
-        mallParking.add(truck);
-        mallParking.add(car);
-        assertThat(mallParking.getTrucks()).isEqualTo(List.of(truck));
-        assertThat(mallParking.getCars()).isEqualTo(List.of(car));
+        assertThat(mallParking.add(truck)).isTrue();
+        assertThat(mallParking.add(car)).isTrue();
     }
 
     @Test
-    public void whenAddTwoTrucksIsSuccessful() {
-        MallParking mallParking = new MallParking(2, 1);
-        Vehicle truck = new Truck("Truck", 2);
-        Vehicle truck1 = new Truck("Truck1", 3);
-        mallParking.add(truck);
-        mallParking.add(truck1);
-        assertThat(mallParking.getTrucks()).isEqualTo(List.of(truck, truck1));
-    }
-
-    @Test
-    public void whenAddTwoCarsIsSuccessful() {
-        MallParking mallParking = new MallParking(1, 2);
-        Vehicle car = new Car("Car");
-        Vehicle car1 = new Car("Car1");
-        mallParking.add(car);
-        mallParking.add(car1);
-        assertThat(mallParking.getCars()).isEqualTo(List.of(car, car1));
-    }
-
-    @Test
-    public void whenAddTrucksAndCarsIsSuccessful() {
-        MallParking mallParking = new MallParking(2, 8);
+    public void whenAddTrucksAndCarIsSuccessful() {
+        MallParking mallParking = new MallParking(1, 3);
         Vehicle truck = new Truck("Truck", 3);
         Vehicle truck1 = new Truck("Truck1", 2);
-        Vehicle truck2 = new Truck("Truck2", 4);
-        Vehicle truck3 = new Truck("Truck3", 2);
         Vehicle car = new Car("Car");
-        Vehicle car1 = new Car("Car1");
         mallParking.add(truck);
         mallParking.add(truck1);
         mallParking.add(car);
-        mallParking.add(truck2);
-        mallParking.add(truck3);
-        mallParking.add(car1);
-        assertThat(mallParking.getTrucks()).isEqualTo(List.of(truck, truck1));
-        assertThat(mallParking.getCars()).isEqualTo(List.of(car, truck2, truck3, car1));
+        assertThat(mallParking.getTrucks()).isEqualTo(List.of(truck));
+        assertThat(mallParking.getCars()).isEqualTo(List.of(truck1, car));
+    }
+
+    @Test
+    public void whenAddTrucksIsInvalid() {
+        MallParking mallParking = new MallParking(1, 2);
+        Vehicle truck = new Truck("Truck", 2);
+        Vehicle truck1 = new Truck("Truck", 3);
+        assertThat(mallParking.add(truck)).isTrue();
+        assertThat(mallParking.add(truck1)).isFalse();
+    }
+    @Test
+    public void whenAddCarsIsInvalid() {
+        MallParking mallParking = new MallParking(1, 1);
+        Vehicle car = new Car("Car");
+        Vehicle car1 = new Car("Car1");
+        assertThat(mallParking.add(car)).isTrue();
+        assertThat(mallParking.add(car1)).isFalse();
     }
 
     @Test
